6ee377de09e95d24129ac8318a4b092b3520b565
pete <pete.alonzi@gmail.com>
2020-05-17 22:07:21 -0400

    Add files via upload

--- extraCredit.py
+++ extraCredit.py
@@ -62,19 +62,38 @@ print()
 
 # Extra Credit, just make a histogram for each and pick the most probable
 #plt.style.use('fivethirtyeight')
-fig, ax = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('Extra Credit Plots')
+
+
 
 # the histogram of the data
 num_bins = 20
-n, bins, patches = plt.hist(rollsREG, num_bins, density=1, facecolor='red', alpha=1, histtype='step', linewidth=4,label="d20")
-n, bins, patches = plt.hist(rollsAOD, num_bins, density=1, facecolor='green', alpha=1, histtype='step',linewidth=4,label="Advantage of Disadvantage")
-n, bins, patches = plt.hist(rollsDOA, num_bins, density=1, facecolor='blue', alpha=1, histtype='step',linewidth=4, label="Disadvantage of Advantage")
+n, bins, patches = ax1.hist(rollsREG, num_bins, density=1, facecolor='red', alpha=1, histtype='step', linewidth=2,label="d20")
+n, bins, patches = ax1.hist(rollsAOD, num_bins, density=1, facecolor='green', alpha=1, histtype='step',linewidth=2,label="Advantage of Disadvantage")
+n, bins, patches = ax1.hist(rollsDOA, num_bins, density=1, facecolor='blue', alpha=1, histtype='step',linewidth=2, label="Disadvantage of Advantage")
 
 # make it understandable
-plt.legend(loc=8)
-ax.set_title("Extra Credit")
+ax1.legend(loc=8)
+ax1.set_title("PDF")
 plt.xlabel('result of roll')
 plt.ylabel('probability of result')
+
+
+
+
+
+# the histogram of the data in reverse cumulative 
+num_bins = 20
+n, bins, patches = ax2.hist(rollsREG, num_bins, density=1, facecolor='red', alpha=1, histtype='step', linewidth=2,label="d20", cumulative=-1)
+n, bins, patches = ax2.hist(rollsAOD, num_bins, density=1, facecolor='green', alpha=1, histtype='step',linewidth=2,label="Advantage of Disadvantage", cumulative=-1)
+n, bins, patches = ax2.hist(rollsDOA, num_bins, density=1, facecolor='blue', alpha=1, histtype='step',linewidth=2, label="Disadvantage of Advantage", cumulative=-1)
+
+# make it understandable
+ax2.legend(loc='upper right')
+ax2.set_title("Reverse CDF (aka probability to roll N or better")
+plt.xlabel('result of roll')
+plt.ylabel('reverse cumulative probability')
 
 
 
